04373fe1b2af539ecab6c7706b48cb18c721f364
hai666l <0xconr@protonmail.com>
2020-09-25 20:40:26 -0500

    ring_buffer.py + tests passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,15 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.index = 0
+        self.list = []
 
     def append(self, item):
-        pass
+        if self.capacity > 0:                           # If list not empty
+            self.list.insert(self.index, item)          # insert new item into list @ index
+            if len(self.list) > self.index+1:           # if we are replacing an item
+                self.list.pop(self.index+1)             # pop it from the list @ index+1 since it has been bumped over by the insert()
+            self.index = (self.index+1) % self.capacity # one-liner to increment or reset self.index
 
     def get(self):
-        pass
+        return self.list
